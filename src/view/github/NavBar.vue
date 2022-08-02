cf51a5eedaf1e99391277a7dac66bbd5e12fa4cd
saivikasm <saivikasmanda12@gmail.com>
2022-08-02 07:45:49 +0530

    Search by name

---
+++ src/view/github/NavBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand>Github Userboard</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="m-auto">
+          <b-nav-form>
+            <b-form-input
+              type="text"
+              size="sm"
+              v-model="searchValue"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+            <b-button
+              size="sm"
+              class="my-2 my-sm-0"
+              type="button"
+              @click="$emit('setSearchValue', searchValue)"
+              >Search</b-button
+            >
+          </b-nav-form>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchValue: "",
+    };
+  },
+};
+</script>
--- src/view/github/UsersList.vue
+++ src/view/github/UsersList.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <NavBar @setSearchValue="setSearchValue" />
     <b-row>
-      <b-col v-for="(user, index) in users" :key="index">
+      <b-col v-for="(user, index) in filteredItems" :key="index">
         <b-img
           thumbnail
           fluid
@@ -20,14 +21,31 @@
 </template>
 <script>
 import User from "@/js/core/user";
+import NavBar from "./NavBar.vue";
 export default {
+  components: { NavBar },
   data() {
     return {
       users: [],
+      searchValue: "",
     };
   },
   async created() {
     this.users = await User.getUsers();
+  },
+  computed: {
+    filteredItems() {
+      return this.users.filter((user) => {
+        return (
+          user.login.toLowerCase().indexOf(this.searchValue.toLowerCase()) != -1
+        );
+      });
+    },
+  },
+  methods: {
+    setSearchValue(value) {
+      this.searchValue = value;
+    },
   },
 };
 </script>
